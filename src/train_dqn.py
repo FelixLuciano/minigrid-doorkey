5a38a6037fea437b6e0a2e0f33e049d263f11783
gianvr <gvgiancarlo1@gmail.com>
2024-05-22 09:44:50 -0300

    adding multiple executions

--- src/train_dqn.py
+++ src/train_dqn.py
@@ -7,31 +7,32 @@ import gymnasium as gym
 
 from CustomRewardWrapper import CustomRewardWrapper
 
-results_path = "./results/dqn"
+for i in range(5):
+    results_path = f"./results/dqn/{i}"
 
-new_logger = configure(results_path, ["stdout", "csv", "tensorboard"])
+    new_logger = configure(results_path, ["stdout", "csv", "tensorboard"])
 
-policy_kwargs = dict(
-    features_extractor_class=MinigridFeaturesExtractor,
-    features_extractor_kwargs=dict(features_dim=128),
-)
+    policy_kwargs = dict(
+        features_extractor_class=MinigridFeaturesExtractor,
+        features_extractor_kwargs=dict(features_dim=128),
+    )
 
-env = gym.make("MiniGrid-DoorKey-6x6-v0", render_mode="rgb_array")
-env = ImgObsWrapper(env)
-env = CustomRewardWrapper(env)
-env = PositionBonus(env)
+    env = gym.make("MiniGrid-DoorKey-6x6-v0", render_mode="rgb_array")
+    env = ImgObsWrapper(env)
+    env = CustomRewardWrapper(env)
+    env = PositionBonus(env)
 
-model = DQN("CnnPolicy", 
-            env, 
-            policy_kwargs=policy_kwargs, 
-            verbose=0,
-            exploration_fraction=0.001,
-            exploration_final_eps=0.5)
+    model = DQN("CnnPolicy", 
+                env, 
+                policy_kwargs=policy_kwargs, 
+                verbose=0,
+                exploration_fraction=0.001,
+                exploration_final_eps=0.5)
 
-model.set_logger(new_logger)
-model.learn(500_000, progress_bar=True)
-model.save(results_path + "/dqn_minigrid")
+    model.set_logger(new_logger)
+    model.learn(500_000, progress_bar=True)
+    model.save(results_path + "/dqn_minigrid")
 
 
-mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
-print(f'Mean reward: {mean_reward} +/- {std_reward:.2f}')
+    mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
+    print(f'Mean reward: {mean_reward} +/- {std_reward:.2f}')
